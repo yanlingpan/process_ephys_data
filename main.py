8db97f8349a84e8e92f6e19c3844e36bace48cb5
ypan <28582896+yanlingpan@users.noreply.github.com>
2023-12-01 22:18:32 -0500

    simplify main

--- main.py
+++ main.py
@@ -9,14 +9,12 @@ from process import process_trace, process_data
 
 if __name__ == "__main__":
 	data_path = Path(os.getcwd(), 'data', '*.*')
-	output_dir = Path(os.getcwd(), 'processed')
-	for input_path in glob(str(data_path)):
-		fname = Path(input_path).name
-		output_fname = fname.replace("asc", "xlsx").replace("txt", "xlsx")
-		output_path = Path(output_dir, output_fname)
-		if fname.find(".asc") != -1: # trace data
-			process_trace(input_path, output_path)
+	for in_path in glob(str(data_path)):
+		out_fname = Path(in_path).stem + "xlsx"
+		out_path = Path(os.getcwd(), 'output', out_fname)
+		if in_path.find(".asc") != -1: # trace data
+			process_trace(in_path, out_path)
 		else:
-			process_data(input_path, output_path)
+			process_data(in_path, out_path)
 
-		print(f"proccessed\t{fname}")
+		print(f"proccessed\t{Path(in_path).name}")
