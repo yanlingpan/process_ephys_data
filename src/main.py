2e1236f6ed1a25e6b22c066fa90cad6b201bc6c0
ypan <28582896+yanlingpan@users.noreply.github.com>
2023-12-04 20:30:01 -0500

    remove executable data folder dependency

--- src/main.py
+++ src/main.py
@@ -8,24 +8,23 @@ from process import process_trace, process_data
 
 
 if __name__ == "__main__":
-	# Get the path of the current file (script)
-	script_path = os.path.realpath(__file__)
-	# Get the directory containing the current file (script)
-	script_dir = os.path.dirname(script_path)
-	# Change the current working directory to the directory containing the current file (script)
-	os.chdir(script_dir)
-
-	if os.path.isdir(Path(os.getcwd(), 'data')):
-		data_path = Path(os.getcwd(), 'data', '*.*')
-		output_path = Path(os.getcwd(), 'output')
+	# find data directory: recursively traverse up tree
+	data_dir = Path(__file__).resolve().parent
+	for _ in range(5):
+		try:
+			assert os.path.isdir(Path(data_dir, 'data'))
+		except AssertionError:
+			data_dir = Path(data_dir, '..')
+	if os.path.isdir(Path(data_dir, 'data')):
+		data_path = Path(data_dir, 'data', '*.*')
+		if ~os.path.isdir(output_path:=Path(data_dir, 'output')):
+			Path(data_dir, 'output').mkdir(parents=False, exist_ok=True)
 	else:
-		data_path = Path(os.getcwd(), '..', 'data', '*.*')
-		output_path = Path(os.getcwd(), '..', 'output')
-	# print("data dir", os.path.isdir(Path(os.getcwd(), 'data')))
-	# data_path = Path(os.getcwd(), '..', 'data', '*.*')
+		print(f"data directory not found: {data_dir}")
+		sys.exit(-1)
+	
 	for in_path in glob(str(data_path)):
 		out_fname = Path(in_path).stem + ".xlsx"
-		# out_path = Path(os.getcwd(), '..', 'output', out_fname)
 		out_path = Path(output_path, out_fname)
 		if in_path.find(".asc") != -1: # trace data
 			process_trace(in_path, out_path)
