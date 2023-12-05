9e8458779231fb616c9c67bbf81335b0a24900c0
ypan <28582896+yanlingpan@users.noreply.github.com>
2023-12-05 11:18:06 -0500

    update dir  traverse loop

--- src/main.py
+++ src/main.py
@@ -8,21 +8,23 @@ from process import process_trace, process_data
 
 
 if __name__ == "__main__":
-	# find data directory: recursively traverse up tree
+	# find data directory: recursively traverse up tree maximal n levels
 	data_dir = Path(__file__).resolve().parent
-	for _ in range(5):
-		try:
-			assert os.path.isdir(Path(data_dir, 'data'))
-		except AssertionError:
-			data_dir = Path(data_dir, '..')
-	if os.path.isdir(Path(data_dir, 'data')):
-		data_path = Path(data_dir, 'data', '*.*')
-		if ~os.path.isdir(output_path:=Path(data_dir, 'output')):
+	n = 5
+	while ( not Path(data_dir, 'data').is_dir() and n>0 ):
+		data_dir = Path(data_dir, '..')
+		n -= 1
+	
+	# get data_path for files, make output directory if not exist
+	if (data_dir:=Path(data_dir, 'data')).is_dir():
+		data_path = Path(data_dir, '*.*')
+		if not (output_path:=Path(data_dir, 'output')).is_dir():
 			Path(data_dir, 'output').mkdir(parents=False, exist_ok=True)
 	else:
 		print(f"data directory not found: {data_dir}")
 		sys.exit(-1)
 	
+	# process all files in data_path
 	for in_path in glob(str(data_path)):
 		out_fname = Path(in_path).stem + ".xlsx"
 		out_path = Path(output_path, out_fname)
